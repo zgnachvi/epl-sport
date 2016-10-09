0278351b34805c0c8b5996722e893b6141c3256a
nikolozgrdzelidze@gmail.com <nikolozgrdzelidze@gmail.com>
2016-10-10 01:03:06 +0400

    tests

--- src/test/java/rss/URLregularExpressionTest.java
+++ src/test/java/rss/URLregularExpressionTest.java
@@ -1,7 +1,15 @@
 package rss;
 
-/**
- * Created by niko on 10/10/16.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
 public class URLregularExpressionTest {
+    @Test
+    public void test(){
+        String reg = "^(http)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+        Assert.assertTrue("http://golazogoal.com/feed/".matches(reg));
+
+        Assert.assertFalse("golazogoal.com/feed/".matches(reg));
+    }
 }
